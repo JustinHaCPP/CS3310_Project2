021baeaec4fd4bb2ca31d31252e268b8876445e3
kayuun <kayuun.business@gmail.com>
2023-04-10 17:48:49 -0700

    Fixed issue with not returning empty anagram sets.

--- Project2.java
+++ Project2.java
@@ -1,3 +1,4 @@
+
 /**************************************************************/
 /* Justin Ha                                                  */
 /* Login ID: 011706708                                        */
@@ -12,22 +13,20 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.LinkedList;
 
-public class Project2
-{
+public class Project2 {
     /**************************************************************/
-    /* Method: Main                                               */
-    /* Purpose: Prints a set of possible anagrams for each word   */
-    /* in an input file using a given dictionary                  */
-    /* Parameters:                                                */
-    /* Command line arguments                                     */
+    /* Method: Main */
+    /* Purpose: Prints a set of possible anagrams for each word */
+    /* in an input file using a given dictionary */
+    /* Parameters: */
+    /* Command line arguments */
     /**************************************************************/
-    public static void main(String[] args) throws FileNotFoundException
-    {
+    public static void main(String[] args) throws FileNotFoundException {
         if (args.length == 2) {
-            String[] dictionary = readDictData(args[0]);           //Get dictionary from readData function
-            LinkedList<String> inFile = readFileData(args[1]);     //Get input file from readData function
+            String[] dictionary = readDictData(args[0]); // Get dictionary from readData function
+            LinkedList<String> inFile = readFileData(args[1]); // Get input file from readData function
             Anagram anagram = new Anagram(dictionary);
-            anagram.transformArray();                              //Transforms the dictionary into a HashMap to be used for key comparisons
+            anagram.transformArray(); // Transforms the dictionary into a HashMap to be used for key comparisons
             for (String x : inFile) {
                 System.out.println(x + ": " + anagram.anagram(x));
             }
@@ -37,31 +36,30 @@ public class Project2
     }
 
     /**************************************************************/
-    /* Method: readDictData                                       */
-    /* Purpose: Reads data from a dictionary file and stores as   */
-    /* an array of String                                         */
-    /* Parameters:                                                */
-    /* String dictName: Path of the dictionary file               */
-    /* Returns: String[] array of String                          */
+    /* Method: readDictData */
+    /* Purpose: Reads data from a dictionary file and stores as */
+    /* an array of String */
+    /* Parameters: */
+    /* String dictName: Path of the dictionary file */
+    /* Returns: String[] array of String */
     /**************************************************************/
-    public static String[] readDictData(String dictName) throws FileNotFoundException
-    {
+    public static String[] readDictData(String dictName) throws FileNotFoundException {
         try {
             File file = new File(dictName);
             Scanner fileReader = new Scanner(file);
             LinkedList<String> linkedList = new LinkedList<String>();
-            int size = 0;                             //number of lines read from dictionary
+            int size = 0; // number of lines read from dictionary
 
-            //Read data
-            while (fileReader.hasNext()) {
-                String value = fileReader.next();     //get string from line and store in linked list
+            // Read data
+            while (fileReader.hasNextLine()) {
+                String value = fileReader.nextLine(); // get string from line and store in linked list
                 linkedList.add(value);
                 size++;
             }
             fileReader.close();
             if (size != 0) {
                 String[] result = new String[size];
-                linkedList.toArray(result);           //convert dictionary linkedlist to array to sort later
+                linkedList.toArray(result); // convert dictionary linkedlist to array to sort later
                 return result;
             } else {
                 System.out.println("Error: Empty dictionary file");
@@ -76,24 +74,23 @@ public class Project2
     }
 
     /**************************************************************/
-    /* Method: readFileData                                       */
-    /* Purpose: Reads data from an input file and stores as       */
-    /* an array of String                                         */
-    /* Parameters:                                                */
-    /* String fileName: Path of the input file                    */
-    /* Returns: LinkedList<String> linked list of String          */
+    /* Method: readFileData */
+    /* Purpose: Reads data from an input file and stores as */
+    /* an array of String */
+    /* Parameters: */
+    /* String fileName: Path of the input file */
+    /* Returns: LinkedList<String> linked list of String */
     /**************************************************************/
-    public static LinkedList<String> readFileData(String fileName) throws FileNotFoundException
-    {
+    public static LinkedList<String> readFileData(String fileName) throws FileNotFoundException {
         try {
             File file = new File(fileName);
             Scanner fileReader = new Scanner(file);
             LinkedList<String> linkedList = new LinkedList<String>();
-            int size = 0;                           //number of lines read from input file
+            int size = 0; // number of lines read from input file
 
-            //Read data
-            while (fileReader.hasNext()) {
-                String value = fileReader.next();   //get string from line and store in linked list
+            // Read data
+            while (fileReader.hasNextLine()) {
+                String value = fileReader.nextLine(); // get string from line and store in linked list
                 linkedList.add(value);
                 size++;
             }
